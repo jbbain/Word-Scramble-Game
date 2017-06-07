eba6972ae6543f1595fc68835d38e399e5190bde
Jared Bain <jbbain22@gmail.com>
2017-06-07 09:38:09 -0500

    WebSockets
    Java program displaying client-server skills. Game scrambles words and opponent has to guess the word.

---
+++ scrambleClient/WordgameClientEndpoint.java
@@ -0,0 +1,71 @@
+package scrambleClient;
+import java.io.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.logging.Logger;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.websocket.CloseReason;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.ClientEndpoint;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.CloseReason.CloseCodes;
+import javax.websocket.server.ServerEndpoint;
+import javax.websocket.DeploymentException;
+
+ 
+
+
+import org.glassfish.tyrus.client.ClientManager;
+
+@ClientEndpoint
+public class WordgameClientEndpoint {
+ 
+    private static CountDownLatch latch;
+ 
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+ 
+    @OnOpen
+    public void onOpen(Session session) {
+    	   logger.info("Connected ... " + session.getId());
+           try {
+               session.getBasicRemote().sendText("start");
+           } catch (IOException e) {
+               throw new RuntimeException(e);
+           }
+    }
+ 
+    @OnMessage
+    public String onMessage(String message, Session session) {
+    	   BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+           try {
+               logger.info("Received ...." + message);
+               String userInput = bufferRead.readLine();
+               return userInput;
+           } catch (IOException e) {
+               throw new RuntimeException(e);
+           }
+    }
+ 
+    @OnClose
+    public void onClose(Session session, CloseReason closeReason) {
+        logger.info(String.format("Session %s close because of %s", session.getId(), closeReason));
+        latch.countDown();
+    }
+ 
+    public static void main(String[] args) {
+        latch = new CountDownLatch(1);
+ 
+        ClientManager client = ClientManager.createClient();
+        try {
+            client.connectToServer(WordgameClientEndpoint.class, new URI("ws://localhost:8025/websockets/game"));
+            latch.await();
+ 
+        } catch (DeploymentException| URISyntaxException | InterruptedException | IOException  e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
